fd772c224f3f1f52b24e10c4bfa707672d04c4aa
VZyma <112830236+VZyma@users.noreply.github.com>
2024-01-31 01:33:49 +0200

    Summary

--- js/modal.js
+++ js/modal.js
@@ -1,14 +1,35 @@
 (() => {
-  const refs = {
-    openModalBtn: document.querySelector('[data-modal-open]'),
-    closeModalBtn: document.querySelector('[data-modal-close]'),
-    modal: document.querySelector('[data-modal]'),
-  };
+    const refs = {
+        openModalBtn: document.querySelectorAll("[data-modal-open]"),
+        closeModalBtn: document.querySelector("[data-modal-close]"),
+        modal: document.querySelector("[data-modal]"),
+    };
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
+    refs.openModalBtn.forEach((element) => {
+        element.addEventListener("click", toggleModal);
+    });
+    refs.closeModalBtn.addEventListener("click", toggleModal);
 
-  function toggleModal() {
-    refs.modal.classList.toggle('is-hidden');
-  }
+    //* Закривання по кліку на бекдроп
+
+    refs.modal.addEventListener("click", removeModal);
+    function removeModal(e) {
+        if (e.target === e.currentTarget) {
+            refs.modal.classList.add("is-hidden");
+        }
+    }
+
+    function toggleModal() {
+        refs.modal.classList.toggle("is-hidden");
+        document.body.classList.toggle("no-scroll");
+	}
+	
+	document.querySelector(".order-button").addEventListener("click", toggleOrderServiceModal);
+	document.querySelector(".modal-close").addEventListener("click", toggleOrderServiceModal);
+
+	function toggleOrderServiceModal() {
+        document.querySelector(".backdrop").classList.toggle("is-open");
+        document.body.classList.toggle("no-scroll");
+	}
+	
 })();
